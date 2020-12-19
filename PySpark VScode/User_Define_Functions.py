5201a3a0e7f5c2342f897ef5e6e5a5e18f1d12aa
Guy-Marcel MBULA BAKU <gmbula@yahoo.fr>
2020-12-19 15:03:02 +0100

    Adding Data and continue code

--- PySpark VScode/User_Define_Functions.py
+++ PySpark VScode/User_Define_Functions.py
@@ -1,17 +1,17 @@
 ##############################################################################################################################
-# Ce chapitre traite les UDF en PySpark
+# Ce chapitre traite les UDF en PySpark. 
+# Les UDF sont très utiles en Spark SQL
 # User Defined Function ou UDF permet à un utilisateur de créer sa propre fonction en python, 
-# de l'enregistrer avec pyspark SQL
-# et de l'utiliser sur des Dataframe ou en Spark SQL
+# de l'enregistrer avec spark SQL et de l'utiliser sur des Dataframe ou en Spark SQL grâce  à udf()
+# Par exemple , on peut essayer de convertir la première lettre d'un mot en majuscule. 
+# En Pyspark, il n'exite pas de fonction permetant de le faire. Il faut donc écri
 #################################################################################################################################
 # import 
 from pyspark.sql import SparkSession
-
 
 #Initialisation 
 spark = SparkSession.builder.appName('UDF en Spark').getOrCreate()
 
 # importer un csv en dataframe 
-
-df = spark.read.csv ('../Data/cereal.csv')
+df = spark.read.csv ('Data/cereal.csv', header=True, sep =';', inferSchema=True)
 df.show()
--- PySpark VScode/spark_read.py
+++ PySpark VScode/spark_read.py
@@ -10,16 +10,39 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('Spark read').getOrCreate()
 
 ##### La syntaxe : 
+
 #### Lire un csv
-### spark.read.csv('path_to_csv') ou spark.read.format("csv").load("path") : renvoie un dataframe
+### spark.read.csv('path_to_csv') : le plus simple et le plus utilisé
+#  ou spark.read.format("csv").load("path") : renvoie un dataframe
+#*************************************************************************************************
+
 # Ici les colonnes seront nommées de _C0 à _C1
 # Les colonnes sont de types strings par défaut 
-df_csv1 = spark.read.csv('/Data/cereal.csv')
-df_csv2 = spark.read.format("csv").load("/Data/cereal.csv")
+df_csv1 = spark.read.csv('Data/cereal.csv', header=True, sep =';')
 
-### Les options
+df_csv2 = spark.read.format("csv").load("Data/cereal.csv")
+
+### Les options (liste non exhaustive)
 # header : noms des colonnes sur la première ligne
-df_csv1 = spark.read.option('header', 'true').csv('../Data/cereal.csv') # Lire les noms de colonnes sur la première ligne
-df_csv2 =  spark.read.format("csv").options(header='true').load('Data/cereal.csv')
-df_csv2.show()
+# schema : un schema de type StrucType 
+# InferSchema : inférer le schemas automatiquement
 
+## Plusieurs options : le plus simple 
+df = spark.read.csv('Data/cereal.csv', header=True, sep =';', inferSchema= True)
+df.show()
+
+
+
+#Soit avec .option : pas très pratique
+df_csv1 = spark.read.option('header', 'true').csv('Data/cereal.csv') # Lire les noms de colonnes sur la première ligne
+df_csv2 =  spark.read.format("csv").options(header='true').load('Data/cereal.csv')
+
+#### To Do : lire plusieur csv en même temps 
+# df = spark.read.csv("path1,path2,path3")
+# df = spark.read.csv("Folder path")
+
+#### Lire un json
+### spark.read.json('path_to_json') ou spark.read.format("json").load("path") : renvoie un dataframe
+#***************************************************************************************************
+
+# Todo: code pour json , txt 
